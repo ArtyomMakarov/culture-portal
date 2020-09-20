b6a93a90e803fda367a726ab045eca94d61658d1
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-09-20 21:19:47 +0300

    add map box module, map component with personal token

--- src/app/poets-list/poets-list.module.ts
+++ src/app/poets-list/poets-list.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { PoetsListRoutingModule } from './poets-list-routing.module';
 import { PoetsListComponent } from './poets-list.component';
+import { MapComponent } from './components/map/map.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
-  declarations: [PoetsListComponent],
-  imports: [CommonModule, PoetsListRoutingModule],
+  declarations: [PoetsListComponent, MapComponent],
+  imports: [
+    CommonModule,
+    PoetsListRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoibmlrYmVsNzciLCJhIjoiY2szaW11bjdkMDk4aTNscWo2aWpza2l6bCJ9.Dl4UkQcw5vsk5krKbIZ2KQ'
+    })
+  ],
   exports: [PoetsListComponent],
 })
 export class PoetsListModule {}
