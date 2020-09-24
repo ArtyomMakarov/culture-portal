f0019c939ac4433208e5f3c58ea5999efc31f8ed
Pavel Saikov <pavsaikov@gmail.com>
2020-09-24 14:57:21 +0300

    feat: implement video component

--- src/app/poets-list/poets-list.module.ts
+++ src/app/poets-list/poets-list.module.ts
@@ -6,14 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MglTimelineModule } from 'angular-mgl-timeline';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
 
 import { SharedModule } from '../shared/shared.module';
 import { AuthorDetailComponent } from './components/author-detail/author-detail.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { MapComponent } from './components/map/map.component';
+import { VideoComponent } from './components/video/video.component';
 import { SearchAuthorPipe } from './pipes/search-author.pipe';
 import { PoetsListRoutingModule } from './poets-list-routing.module';
 import { PoetsListComponent } from './poets-list.component';
+import { VideoIdPipe } from './pipes/id.pipe';
 
 @NgModule({
   declarations: [
@@ -22,6 +25,8 @@ import { PoetsListComponent } from './poets-list.component';
     SearchAuthorPipe,
     AuthorDetailComponent,
     GalleryComponent,
+    VideoComponent,
+    VideoIdPipe,
   ],
   imports: [
     SharedModule,
@@ -36,6 +41,7 @@ import { PoetsListComponent } from './poets-list.component';
         'pk.eyJ1IjoibmlrYmVsNzciLCJhIjoiY2szaW11bjdkMDk4aTNscWo2aWpza2l6bCJ9.Dl4UkQcw5vsk5krKbIZ2KQ',
     }),
     NgImageSliderModule,
+    NgxYoutubePlayerModule,
   ],
   exports: [PoetsListComponent],
 })
