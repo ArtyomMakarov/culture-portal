cdf54c3bc1f88d5d69329778627c1364a6289c21
NIkita Belyakov <nikita.belyakov.38@ya.ru>
2020-09-25 19:54:33 +0300

    add some style to poets-list

--- src/app/poets-list/components/poets-list/poets-list.component.ts
+++ src/app/poets-list/components/poets-list/poets-list.component.ts
@@ -6,6 +6,15 @@ import { Subscription } from 'rxjs';
 import { IAuthor } from '../../../core/models/authors-model';
 import { langs } from '../../../core/models/langs.model';
 import { AuthorsService } from '../../../core/services/authors.service';
+import {
+  trigger,
+  transition,
+  style,
+  query,
+  group,
+  animateChild,
+  animate,
+} from '@angular/animations';
 
 interface Iphoto {
   photo: string;
@@ -15,19 +24,27 @@ interface Iphoto {
   selector: 'app-poets-list',
   templateUrl: './poets-list.component.html',
   styleUrls: ['./poets-list.component.scss'],
+  animations: [
+    trigger('show', [
+      transition(':enter', [
+        style({ transform: 'translate(30%) scale(1.4)', opacity: 0.5 }),
+        animate('0.5s ease-out')
+      ])
+    ])
+  ]
 })
 export class PoetsListComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
   public authorsList: IAuthor[];
   public keyWord: string;
-  public titleImgAuthor: string = './assets/img/poets-img/img0_orig.jpg';
+  public selected: string;
   public nameRoutePath: string;
 
   constructor(
     private authors: AuthorsService,
     public translate: TranslateService,
     public router: Router
-  ) {}
+  ) { }
 
   public inputWords(query: string): void {
     query = query.trim();
@@ -36,8 +53,8 @@ export class PoetsListComponent implements OnInit, OnDestroy {
     }
   }
 
-  public selectAuthor(item: Iphoto): void {
-    this.titleImgAuthor = item.photo;
+  public selectAuthor(id: string): void {
+    this.selected = id;
   }
 
   public goToDetailedPage(name: string): void {
